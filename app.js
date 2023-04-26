de188fd3739cb74d11e8864d1d6b735859492381
wasifAhmed15 <wasifahmedkhan99@gmail.com>
2023-04-26 19:01:39 +0500

    paratice in javascript

---
+++ app.js
@@ -0,0 +1,130 @@
+
+// chapter#13
+// var Age = prompt("Enter Your Age","Your Age is" )
+// var gender = prompt("Enter Your Gender","Male & Female & Other")
+// if(Age >= 18 && gender == "male"){
+//     alert("Your Alow This drive a car ")
+// }
+// else(
+//     alert("You are not allow")
+// )
+
+// var Age = prompt("Enter Your Age","Your Age is" )
+// var gender = prompt("Enter Your Gender","Male & Female & Other")
+// if(Age >= 18 || gender == "male"){
+//     alert("Your Alow This drive a car ")
+// }
+// else(
+//     alert("You are not allow")
+// )
+
+// var x=5
+// var y=10
+// var a=30
+// var b=15
+
+// if(x+y === a-b){
+//     alert("correct")
+// }
+// else(
+//     alert("incorrect")
+// )
+
+// MarkSheet
+// var English = +prompt("Enter Your English subject marks","English Marks")
+// var urdu = +prompt("Enter Your urdu subject marks","urdu Marks")
+// var Science = +prompt("Enter Your science subject marks","science Marks")
+// var Math = +prompt("Enter Your Math subject marks","Math Marks")
+// var Islamiyat = +prompt("Enter Your Islamiyat subject marks","Islamiyat Marks")
+
+// var total = English+urdu+Science+Math+Islamiyat
+// if (total>=400 && total<=500) {
+//     alert("A+")
+// }
+// else if(total>=300 && total<=400){
+//     alert("A")
+// }
+
+// else if(total>=200 && total<=300){
+//     alert("B")
+// }
+
+// else if(total>=100 && total<=200){
+//     alert("C")
+// }
+// else if(total>=50 && total<=100){
+//     alert("Fail")
+// }
+
+// else(alert("Absent"))
+
+// Marksheet  10 class
+
+// var Mark = +prompt("Enter YOUR 10 class percentage")
+// if (Mark>=80 && Mark <=100) {
+//     document.write("A+ GRADE")
+// }
+// else if(Mark>=70 && Mark <=80){
+//     document.write("A GRADE")
+// }
+// else if(Mark>=60 && Mark <=70){
+//     document.write("B GRADE")
+// }
+// else if(Mark>=50 && Mark <=60){
+//     document.write("C GRADE")
+// }
+// else if(Mark>=40 && Mark <=50){
+//     document.write("D GRADE")
+// }
+
+// else if(Mark>=30 && Mark <=40){
+//     document.write("FAIL")
+// }
+// else(document.write("Absent"))
+
+
+// Array
+
+
+// var student =["wasif","ahmed","khan"]
+// console.log(student)
+
+// var student =["wasif","ahmed","khan"]
+// console.log(student[0])
+
+// var student =["wasif","ahmed","khan"]
+// student.pop()
+// console.log(student)
+
+// var student =["wasif","ahmed","khan"]
+// student.push("Atif","Ahmed")
+// console.log(student)
+
+
+// var student =["wasif","ahmed","khan"]
+// student.shift()
+// console.log(student)
+
+
+// var student =["wasif","ahmed","khan"]
+// student.unshift("Atif","Ahmed")
+// console.log(student)
+
+
+// var student =["wasif","ahmed","khan"]
+// student.splice(0,1,"Atif")
+// // target the value , delete the value,"add the value"
+// console.log(student)
+
+
+// var student = ["wasif","ahmed","khan"]
+// student.splice(0,0,"Atif")
+
+
+// var student = ["wasif","Ahmed","khan"
+//                 ,"Atif","Ahmed","khan"
+              
+
+// ]
+// var copy = student.slice(3,6)
+// console.log(copy)
